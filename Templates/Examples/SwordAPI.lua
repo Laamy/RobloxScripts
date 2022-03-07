10056c351d2c5379db7cc11447066a38a7d79ef9
Laamy <83557555+Laamy@users.noreply.github.com>
2022-03-07 17:37:02 +1100

    Create SwordAPI.lua

---
+++ Templates/Examples/SwordAPI.lua
@@ -0,0 +1,6 @@
+local swordAPI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Laamy/RobloxScripts/main/Utils/Sword_Module.lua"))()
+swordAPI.setSwordName("Sword") -- configurate
+
+swordAPI.equipSword()
+wait()
+swordAPI.KillAll() -- kill all players
