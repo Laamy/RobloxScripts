ed1105b80fde2ed9f592a725994fd8c8b80b93b5
Laamy <83557555+Laamy@users.noreply.github.com>
2022-03-14 13:57:07 +1100

    Update AIModule.lua

--- Utils/AIModule.lua
+++ Utils/AIModule.lua
@@ -7,49 +7,62 @@ function AIModule:StopAI()
 	__endScriptB = true
 end
 
-function AIModule:StartAI(character, part)
+function AIModule:StartAI(character, part, dbs)
 	__endScriptB = false
-	AIModule:Follow(character, part)
+	AIModule:Follow(character, part, dbs)
 end
 
-function AIModule:Follow(character, part)
-	local canContinue = true
-	while canContinue == true and not __endScriptB do wait(0.1)
-		local path = pf:CreatePath()
+function AIModule:Follow(character, part, dbs)
+	pcall(function()
+		local canContinue = true
+		while canContinue == true and not __endScriptB do wait(0.1)
+			local path = pf:CreatePath()
 
-		local startp = character.HumanoidRootPart.Position
-		local endp = part.Position
+			local startp = character.HumanoidRootPart.Position
+			local endp = part.Position
 
-		path:ComputeAsync(startp, endp)
-		
-		for i, wp in pairs(path:GetWaypoints()) do
-			local cpos = character.HumanoidRootPart.Position
-			spawn(function()
-				wait(1)
-				local magnitude = (character.HumanoidRootPart.Position - cpos).Magnitude
-				--print(magnitude)
-				if magnitude < 5 then
-					canContinue = false
-					AIModule:Follow(character, part)
-				elseif magnitude > 24 then
-					canContinue = false
+			path:ComputeAsync(startp, endp)
+
+			for i, wp in pairs(path:GetWaypoints()) do
+				local magnitude = (character.HumanoidRootPart.Position - endp).Magnitude
+				if magnitude < dbs then
+					break
 				end
-			end)
-			
-			if __endScriptB or not canContinue then
-				canContinue = false
-				break
-			end
-			
-			character.Humanoid:MoveTo(wp.Position)
 
-			if wp.Action == Enum.PathWaypointAction.Jump then
-				character.Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
-			end
+				local cpos = character.HumanoidRootPart.Position
+				spawn(function()
+					wait(1)
+					local magnitude = (character.HumanoidRootPart.Position - cpos).Magnitude
+					--print(magnitude)
+					if magnitude < 5 then
+						canContinue = false
+						AIModule:Follow(character, part, dbs)
+					elseif magnitude > 24 then
+						canContinue = false
+					end
+				end)
 
-			character.Humanoid.MoveToFinished:Wait()
+				if __endScriptB or not canContinue then
+					canContinue = false
+					break
+				end
+
+				character.Humanoid:MoveTo(wp.Position)
+
+				if wp.Action == Enum.PathWaypointAction.Jump then
+					character.Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
+				end
+
+				character.Humanoid.MoveToFinished:Wait()
+			end
 		end
-	end
+	end)
 end
 
 return AIModule
+
+-- Initialize the AI
+-- AIModule:StartAI(script.Parent, workspace.Parts.Part, 8)
+
+-- Stop AI
+-- AIModule:StopAI()
