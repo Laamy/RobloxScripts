33ee102e75298a98f4a8a53dd4bf6e49b6bb4ae0
Laamy <83557555+Laamy@users.noreply.github.com>
2022-05-11 20:32:30 +1000

    Create Device Adapter Pads.lua

---
+++ RobloxGames/Kohls/Device Adapter Pads.lua
@@ -0,0 +1,420 @@
+-- Instances:
+
+local DeviceAdapter_Pads = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local UICorner = Instance.new("UICorner")
+local Draggable = Instance.new("Frame")
+local UICorner_2 = Instance.new("UICorner")
+local capFrame = Instance.new("Frame")
+local mainFrame = Instance.new("Frame")
+local UICorner_3 = Instance.new("UICorner")
+local mainFrame_2 = Instance.new("Frame")
+local UICorner_4 = Instance.new("UICorner")
+local UIGridLayout = Instance.new("UIGridLayout")
+local pad1 = Instance.new("TextButton")
+local UICorner_5 = Instance.new("UICorner")
+local pad2 = Instance.new("TextButton")
+local UICorner_6 = Instance.new("UICorner")
+local pad3 = Instance.new("TextButton")
+local UICorner_7 = Instance.new("UICorner")
+local pad4 = Instance.new("TextButton")
+local UICorner_8 = Instance.new("UICorner")
+local pad5 = Instance.new("TextButton")
+local UICorner_9 = Instance.new("UICorner")
+local pad6 = Instance.new("TextButton")
+local UICorner_10 = Instance.new("UICorner")
+local pad7 = Instance.new("TextButton")
+local UICorner_11 = Instance.new("UICorner")
+local pad8 = Instance.new("TextButton")
+local UICorner_12 = Instance.new("UICorner")
+local pad9 = Instance.new("TextButton")
+local UICorner_13 = Instance.new("UICorner")
+local TextButton = Instance.new("TextButton")
+local UICorner_14 = Instance.new("UICorner")
+local TextButton_2 = Instance.new("TextButton")
+local UICorner_15 = Instance.new("UICorner")
+
+--Properties:
+
+DeviceAdapter_Pads.Name = "DeviceAdapter_Pads"
+DeviceAdapter_Pads.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+
+Frame.Parent = DeviceAdapter_Pads
+Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame.BackgroundTransparency = 1.000
+Frame.BorderSizePixel = 0
+Frame.Position = UDim2.new(0.0772659704, 0, 0.298181832, 0)
+Frame.Size = UDim2.new(0, 524, 0, 26)
+
+UICorner.Parent = Frame
+
+Draggable.Name = "Draggable"
+Draggable.Parent = Frame
+Draggable.BackgroundColor3 = Color3.fromRGB(35, 35, 35)
+Draggable.Position = UDim2.new(-0.00263513671, 0, -0.00950974692, 0)
+Draggable.Size = UDim2.new(0, 526, 0, 113)
+
+UICorner_2.Parent = Draggable
+
+capFrame.Name = "capFrame"
+capFrame.Parent = Draggable
+capFrame.BackgroundColor3 = Color3.fromRGB(44, 44, 44)
+capFrame.BorderSizePixel = 0
+capFrame.Position = UDim2.new(0, 0, 0.235044226, 0)
+capFrame.Size = UDim2.new(0, 526, 0, 9)
+
+mainFrame.Name = "mainFrame"
+mainFrame.Parent = capFrame
+mainFrame.BackgroundColor3 = Color3.fromRGB(44, 44, 44)
+mainFrame.Size = UDim2.new(0, 526, 0, 86)
+
+UICorner_3.Parent = mainFrame
+
+mainFrame_2.Name = "mainFrame"
+mainFrame_2.Parent = mainFrame
+mainFrame_2.BackgroundColor3 = Color3.fromRGB(44, 44, 44)
+mainFrame_2.Position = UDim2.new(0.0348837674, 0, 0.197674423, 0)
+mainFrame_2.Size = UDim2.new(0, 494, 0, 51)
+
+UICorner_4.Parent = mainFrame_2
+
+UIGridLayout.Parent = mainFrame_2
+UIGridLayout.SortOrder = Enum.SortOrder.LayoutOrder
+UIGridLayout.CellSize = UDim2.new(0, 50, 0, 50)
+
+pad1.Name = "pad1"
+pad1.Parent = mainFrame_2
+pad1.BackgroundColor3 = Color3.fromRGB(0, 170, 0)
+pad1.Position = UDim2.new(0.0325581394, 0, 0.209302336, 0)
+pad1.Size = UDim2.new(0, 50, 0, 50)
+pad1.Font = Enum.Font.SourceSans
+pad1.Text = ""
+pad1.TextColor3 = Color3.fromRGB(0, 0, 0)
+pad1.TextSize = 14.000
+
+UICorner_5.Parent = pad1
+
+pad2.Name = "pad2"
+pad2.Parent = mainFrame_2
+pad2.BackgroundColor3 = Color3.fromRGB(0, 170, 0)
+pad2.Position = UDim2.new(0.0325581394, 0, 0.209302336, 0)
+pad2.Size = UDim2.new(0, 50, 0, 50)
+pad2.Font = Enum.Font.SourceSans
+pad2.Text = ""
+pad2.TextColor3 = Color3.fromRGB(0, 0, 0)
+pad2.TextSize = 14.000
+
+UICorner_6.Parent = pad2
+
+pad3.Name = "pad3"
+pad3.Parent = mainFrame_2
+pad3.BackgroundColor3 = Color3.fromRGB(0, 170, 0)
+pad3.Position = UDim2.new(0.0325581394, 0, 0.209302336, 0)
+pad3.Size = UDim2.new(0, 50, 0, 50)
+pad3.Font = Enum.Font.SourceSans
+pad3.Text = ""
+pad3.TextColor3 = Color3.fromRGB(0, 0, 0)
+pad3.TextSize = 14.000
+
+UICorner_7.Parent = pad3
+
+pad4.Name = "pad4"
+pad4.Parent = mainFrame_2
+pad4.BackgroundColor3 = Color3.fromRGB(0, 170, 0)
+pad4.Position = UDim2.new(0.0325581394, 0, 0.209302336, 0)
+pad4.Size = UDim2.new(0, 50, 0, 50)
+pad4.Font = Enum.Font.SourceSans
+pad4.Text = ""
+pad4.TextColor3 = Color3.fromRGB(0, 0, 0)
+pad4.TextSize = 14.000
+
+UICorner_8.Parent = pad4
+
+pad5.Name = "pad5"
+pad5.Parent = mainFrame_2
+pad5.BackgroundColor3 = Color3.fromRGB(0, 170, 0)
+pad5.Position = UDim2.new(0.0325581394, 0, 0.209302336, 0)
+pad5.Size = UDim2.new(0, 50, 0, 50)
+pad5.Font = Enum.Font.SourceSans
+pad5.Text = ""
+pad5.TextColor3 = Color3.fromRGB(0, 0, 0)
+pad5.TextSize = 14.000
+
+UICorner_9.Parent = pad5
+
+pad6.Name = "pad6"
+pad6.Parent = mainFrame_2
+pad6.BackgroundColor3 = Color3.fromRGB(0, 170, 0)
+pad6.Position = UDim2.new(0.0325581394, 0, 0.209302336, 0)
+pad6.Size = UDim2.new(0, 50, 0, 50)
+pad6.Font = Enum.Font.SourceSans
+pad6.Text = ""
+pad6.TextColor3 = Color3.fromRGB(0, 0, 0)
+pad6.TextSize = 14.000
+
+UICorner_10.Parent = pad6
+
+pad7.Name = "pad7"
+pad7.Parent = mainFrame_2
+pad7.BackgroundColor3 = Color3.fromRGB(0, 170, 0)
+pad7.Position = UDim2.new(0.0325581394, 0, 0.209302336, 0)
+pad7.Size = UDim2.new(0, 50, 0, 50)
+pad7.Font = Enum.Font.SourceSans
+pad7.Text = ""
+pad7.TextColor3 = Color3.fromRGB(0, 0, 0)
+pad7.TextSize = 14.000
+
+UICorner_11.Parent = pad7
+
+pad8.Name = "pad8"
+pad8.Parent = mainFrame_2
+pad8.BackgroundColor3 = Color3.fromRGB(0, 170, 0)
+pad8.Position = UDim2.new(0.0325581394, 0, 0.209302336, 0)
+pad8.Size = UDim2.new(0, 50, 0, 50)
+pad8.Font = Enum.Font.SourceSans
+pad8.Text = ""
+pad8.TextColor3 = Color3.fromRGB(0, 0, 0)
+pad8.TextSize = 14.000
+
+UICorner_12.Parent = pad8
+
+pad9.Name = "pad9"
+pad9.Parent = mainFrame_2
+pad9.BackgroundColor3 = Color3.fromRGB(0, 170, 0)
+pad9.Position = UDim2.new(0.0325581394, 0, 0.209302336, 0)
+pad9.Size = UDim2.new(0, 50, 0, 50)
+pad9.Font = Enum.Font.SourceSans
+pad9.Text = ""
+pad9.TextColor3 = Color3.fromRGB(0, 0, 0)
+pad9.TextSize = 14.000
+
+UICorner_13.Parent = pad9
+
+TextButton.Parent = Draggable
+TextButton.BackgroundColor3 = Color3.fromRGB(255, 96, 85)
+TextButton.Position = UDim2.new(0, 5, 0, 5)
+TextButton.Size = UDim2.new(0, 15, 0, 15)
+TextButton.Font = Enum.Font.SourceSans
+TextButton.Text = ""
+TextButton.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextButton.TextSize = 10.000
+
+UICorner_14.CornerRadius = UDim.new(1, 0)
+UICorner_14.Parent = TextButton
+
+TextButton_2.Parent = Draggable
+TextButton_2.BackgroundColor3 = Color3.fromRGB(255, 192, 51)
+TextButton_2.Position = UDim2.new(0, 25, 0, 5)
+TextButton_2.Size = UDim2.new(0, 15, 0, 15)
+TextButton_2.Font = Enum.Font.SourceSans
+TextButton_2.Text = ""
+TextButton_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextButton_2.TextSize = 10.000
+
+UICorner_15.CornerRadius = UDim.new(1, 0)
+UICorner_15.Parent = TextButton_2
+
+-- Scripts:
+
+local function XDTSDBT_fake_script() -- Frame.MgD 
+	local script = Instance.new('LocalScript', Frame)
+
+	local UserInputService = game:GetService("UserInputService")
+	
+	local gui = script.Parent
+	
+	local dragging
+	local dragInput
+	local dragStart
+	local startPos
+	
+	local function update(input)
+		local delta = input.Position - dragStart
+		gui.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+	end
+	
+	gui.InputBegan:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+			dragging = true
+			dragStart = input.Position
+			startPos = gui.Position
+	
+			input.Changed:Connect(function()
+				if input.UserInputState == Enum.UserInputState.End then
+					dragging = false
+				end
+			end)
+		end
+	end)
+	
+	gui.InputChanged:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+			dragInput = input
+		end
+	end)
+	
+	UserInputService.InputChanged:Connect(function(input)
+		if input == dragInput and dragging then
+			update(input)
+		end
+	end)
+end
+coroutine.wrap(XDTSDBT_fake_script)()
+local function HQLRVEH_fake_script() -- TextButton.LocalScript 
+	local script = Instance.new('LocalScript', TextButton)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		script.Parent.Parent.Parent.Parent:Destroy()
+	end)
+end
+coroutine.wrap(HQLRVEH_fake_script)()
+local function TKZO_fake_script() -- TextButton_2.LocalScript 
+	local script = Instance.new('LocalScript', TextButton_2)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		if script.Parent.Parent.capFrame.Visible then
+			script.Parent.Parent.Size = UDim2.new(0, 102,0, 26)
+			script.Parent.Parent.Parent.Size = UDim2.new(0, 102,0, 26)
+			script.Parent.Parent.capFrame.Visible = false
+		else
+			script.Parent.Parent.Size = UDim2.new(0, 526, 0, 113)
+			script.Parent.Parent.Parent.Size = UDim2.new(0, 524, 0, 26)
+			script.Parent.Parent.capFrame.Visible = true
+		end
+	end)
+end
+coroutine.wrap(TKZO_fake_script)()
+
+padOrder = {
+	['pad1'] = 6,
+	['pad2'] = 8,
+	['pad3'] = 4,
+	['pad4'] = 9,
+	['pad5'] = 7,
+	['pad6'] = 3,
+	['pad7'] = 2,
+	['pad8'] = 1,
+	['pad9'] = 5
+}
+padsColour = {
+	['Enabled'] = Color3.fromRGB(75, 151, 75),
+	['Disabled'] = Color3.fromRGB(196, 40, 28)
+}
+
+local gameFlr = workspace.Terrain._Game
+local adminFlr = gameFlr.Admin
+
+local pads = adminFlr.Pads:GetChildren()
+
+pad1.MouseButton1Click:Connect(function()
+	local spr = game.Players.LocalPlayer.Character:FindFirstChild("Head")
+	firetouchinterest(pads[padOrder['pad1']].Head, spr, 1)
+	wait()
+	firetouchinterest(pads[padOrder['pad1']].Head, spr, 0)
+end)
+
+pad2.MouseButton1Click:Connect(function()
+	local spr = game.Players.LocalPlayer.Character:FindFirstChild("Head")
+	firetouchinterest(pads[padOrder['pad2']].Head, spr, 1)
+	wait()
+	firetouchinterest(pads[padOrder['pad2']].Head, spr, 0)
+end)
+
+pad3.MouseButton1Click:Connect(function()
+	local spr = game.Players.LocalPlayer.Character:FindFirstChild("Head")
+	firetouchinterest(pads[padOrder['pad3']].Head, spr, 1)
+	wait()
+	firetouchinterest(pads[padOrder['pad3']].Head, spr, 0)
+end)
+
+pad4.MouseButton1Click:Connect(function()
+	local spr = game.Players.LocalPlayer.Character:FindFirstChild("Head")
+	firetouchinterest(pads[padOrder['pad4']].Head, spr, 1)
+	wait()
+	firetouchinterest(pads[padOrder['pad4']].Head, spr, 0)
+end)
+
+pad5.MouseButton1Click:Connect(function()
+	local spr = game.Players.LocalPlayer.Character:FindFirstChild("Head")
+	firetouchinterest(pads[padOrder['pad5']].Head, spr, 1)
+	wait()
+	firetouchinterest(pads[padOrder['pad5']].Head, spr, 0)
+end)
+
+pad6.MouseButton1Click:Connect(function()
+	local spr = game.Players.LocalPlayer.Character:FindFirstChild("Head")
+	firetouchinterest(pads[padOrder['pad6']].Head, spr, 1)
+	wait()
+	firetouchinterest(pads[padOrder['pad6']].Head, spr, 0)
+end)
+
+pad7.MouseButton1Click:Connect(function()
+	local spr = game.Players.LocalPlayer.Character:FindFirstChild("Head")
+	firetouchinterest(pads[padOrder['pad7']].Head, spr, 1)
+	wait()
+	firetouchinterest(pads[padOrder['pad7']].Head, spr, 0)
+end)
+
+pad8.MouseButton1Click:Connect(function()
+	local spr = game.Players.LocalPlayer.Character:FindFirstChild("Head")
+	firetouchinterest(pads[padOrder['pad8']].Head, spr, 1)
+	wait()
+	firetouchinterest(pads[padOrder['pad8']].Head, spr, 0)
+end)
+
+pad9.MouseButton1Click:Connect(function()
+	local spr = game.Players.LocalPlayer.Character:FindFirstChild("Head")
+	firetouchinterest(pads[padOrder['pad9']].Head, spr, 1)
+	wait()
+	firetouchinterest(pads[padOrder['pad9']].Head, spr, 0)
+end)
+
+local pds = "Touch to get admin"
+
+while true do wait()
+	if pads[padOrder['pad1']].Name == pds then
+		pad1.BackgroundColor3 = padsColour['Enabled']
+	else
+		pad1.BackgroundColor3 = padsColour['Disabled']
+	end
+	if pads[padOrder['pad2']].Name == pds then
+		pad2.BackgroundColor3 = padsColour['Enabled']
+	else
+		pad2.BackgroundColor3 = padsColour['Disabled']
+	end
+	if pads[padOrder['pad3']].Name == pds then
+		pad3.BackgroundColor3 = padsColour['Enabled']
+	else
+		pad3.BackgroundColor3 = padsColour['Disabled']
+	end
+	if pads[padOrder['pad4']].Name == pds then
+		pad4.BackgroundColor3 = padsColour['Enabled']
+	else
+		pad4.BackgroundColor3 = padsColour['Disabled']
+	end
+	if pads[padOrder['pad5']].Name == pds then
+		pad5.BackgroundColor3 = padsColour['Enabled']
+	else
+		pad5.BackgroundColor3 = padsColour['Disabled']
+	end
+	if pads[padOrder['pad6']].Name == pds then
+		pad6.BackgroundColor3 = padsColour['Enabled']
+	else
+		pad6.BackgroundColor3 = padsColour['Disabled']
+	end
+	if pads[padOrder['pad7']].Name == pds then
+		pad7.BackgroundColor3 = padsColour['Enabled']
+	else
+		pad7.BackgroundColor3 = padsColour['Disabled']
+	end
+	if pads[padOrder['pad8']].Name == pds then
+		pad8.BackgroundColor3 = padsColour['Enabled']
+	else
+		pad8.BackgroundColor3 = padsColour['Disabled']
+	end
+	if pads[padOrder['pad9']].Name == pds then
+		pad9.BackgroundColor3 = padsColour['Enabled']
+	else
+		pad9.BackgroundColor3 = padsColour['Disabled']
+	end
+end
